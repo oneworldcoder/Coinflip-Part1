0c1833f9ecb432de78e5c81af1b42a21321d509c
oneworldcoder <52066039+oneworldcoder@users.noreply.github.com>
2020-12-27 19:59:28 +0530

    Add files via upload

--- dapp/main.js
+++ dapp/main.js
@@ -1,16 +1,20 @@
 var web3 = new Web3(Web3.givenProvider);
 var contractInstance;
-var contractAddress = "0x2Ae24960e9f6331e68Ef6c19b7FA15094adFdA98";
+var contractAddress = "0x5E9f0391B590c823db2A8b0D333980F70869C811";
 var number;
 var guess;
 
 $(document).ready(function() {
     window.ethereum.enable().then(function(accounts) {
-      contractInstance = new web3.eth.Contract(window.abi, contractAddress, {from: accounts[0]});
+      contractInstance = new web3.eth.Contract(abi, contractAddress, {from: accounts[0]});
       console.log(contractInstance);
     });
-    $("#bet_0").click(bet(0));
-    $("#bet_1").click(bet(1));
+    $("#bet_0").click( () =>{
+      bet(0)
+    });
+    $("#bet_1").click( () => {
+      bet(1)
+    });
 });
 
 function bet(number) {
@@ -35,7 +39,7 @@ function bet(number) {
 }
 
 async function fetchAndDisplay(guess) {
-  await contractInstance.methods.outcome.call().then(function(res) {
+  await contractInstance.methods.outcome.call().call().then(function(res) {
     if(guess=res){
       console.log("Result is " + res + " you won!");
       $("#outcome_display").text("You Won!");
